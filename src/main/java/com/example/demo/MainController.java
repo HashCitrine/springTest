1c4fbe04fd6718b2b5f212f5df21b009b3f08367
TAEMOKKIM <using1524@naver.com>
2023-02-14 16:28:54 +0900

    add mongodb dependency

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+	@Value("${index}")
+	String index;
+
 	@RequestMapping("/")
 	public String helloWorld() {
-		return "Hello World!";
+		return index;
 	}
 }
